3ca4f062509d879a5d178d52a9abb44f14ab6e53
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2024-05-19 17:45:05 +0000

    I am not proud

---
+++ models/user.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" holds class User"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+class User(BaseModel, Base):
+    """ representation of a user """
+    if models.storage_t == "db":
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user", cascade="all, delete-orphan")
+        reviews = relationship("Review", backref="user", cascade="all, delete-orphan")
+
+    def __init__(self, *args, **kwargs):
+        """initializes Review"""
+        super().__init__(*args, **kwargs)
